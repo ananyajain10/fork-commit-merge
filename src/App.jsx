6434330be55b058a9a02c0564b8db2c2e4f41f39
ananyajain10 <jainananya355@gmail.com>
2023-08-21 17:32:06 +0530

    changes-FIXED

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./index.css";
+function App() {
+  const [meme, setMeme] = useState({
+   
+    memes: "https://i.imgflip.com/8p0a.jpg",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((preValue) => ({
+      ...preValue,
+      [name]: value,
+    }));
+  }
+  const [memeData, setMemeData] = useState([]);
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setMemeData(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * memeData.length);
+    const url = memeData[randomNumber].url;
+    setMeme((preValue) => ({
+      ...preValue,
+      memes: url,
+    }));
+    console.log(url);
+  }
+
+  return (
+    <div className="center">
+      
+      <button className="form-button" onClick={getMemeImage}>
+        Get New MEME 
+      </button>
+      <div className="card">
+
+        <img className="card img" alt="Meme" src={meme.memes} />
+        
+      </div>
+    </div>
+  );
+}
+export default App;
